0d350bb82a06c363780a591dd7833e8fc34c4444
Bob Nicholas <bobby.nicholas@gmail.com>
2023-03-31 19:28:11 -0400

    inject env variables

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import azure from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
